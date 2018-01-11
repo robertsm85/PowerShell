b3fcb45ab7d750bd9b53ca6193b652a79c0aa750
robertsm85 <robertsm85@gmail.com>
2018-01-11 15:35:05 -0600

    Initial commit

---
+++ Start_VPN.ps1
@@ -0,0 +1,22 @@
+﻿#Variables list. Replace variables and/or add more applications as needed.
+[String] $Server = "Redwing"
+[String] $BaseApplication = "vpnui.exe"
+[String] $Application1 = "lync.exe"
+[String] $Application2 = "terminals.exe"
+
+#Starts Cisco VPN Client.
+start-process $BaseApplication
+
+#Tests connection to DC.
+Do {
+$Connected=test-connection -computer $Server -Count 1 -Quiet
+"Attempting to ping Domain Controller."
+}
+
+#Waits for connection to succeed.
+Until ($Connected -eq "True")
+"Ping to Domain Controller sucessful."
+
+#Starts applications.
+start-process $Application1
+start-process $Application2
